e8d6696d8bb1e5baac3f3d5ec7a2a18e0dcbf74b
sassin <jainsachin9095@gmail.com>
2015-06-15 16:13:00 -0400

    Restructured bolts and moved them to respective packages. The code uses window object and commonframework interface

--- examples/storm-starter/src/jvm/storm/starter/CummulativeMovingAvgTopology.java
+++ examples/storm-starter/src/jvm/storm/starter/CummulativeMovingAvgTopology.java
@@ -6,73 +6,19 @@ package storm.starter;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.base.BaseRichBolt;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import storm.starter.HelperClasses.WindowObject;
+import storm.starter.bolt.MovingAverageBolt;
+import storm.starter.bolt.TumblingWindow;
 import storm.starter.spout.RandomIntegerSpout;
 
 import java.io.File;
-import java.util.Map;
-
-//import backtype.storm.testing.TestWordSpout;
 
 public class CummulativeMovingAvgTopology {
     final static Logger LOG = Logger.getLogger(CummulativeMovingAvgTopology.class.getName());
-    //static WindowObject wObject;
-    public static class MovingAverage extends BaseRichBolt {
-        OutputCollector _collector;
-        //static WindowObject wObject;
-        int count;
-        double cma;
-
-        public MovingAverage()
-        {
-            count = 0;
-            cma = 0;
-        }
-
-        @Override
-        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-            _collector = collector;
-
-        }
-
-        @Override
-        public void execute(Tuple tuple) {
-            if (isMockTick(tuple))
-            {
-                LOG.info("~~~Got Mock Tuple");
-                double avg = cma / count;
-                _collector.emit(tuple, new Values(avg));
-                LOG.info("Window Avg is::" + avg + "    Window Total::" + cma + "   Count::" + count);
-                count = 0;
-                cma = 0;
-            }
-            else {
-                cma = cma + tuple.getInteger(0);
-                count++;
-            }
-        }
-
-        @Override
-        public void declareOutputFields(OutputFieldsDeclarer declarer) {
-            declarer.declare(new Fields("Average"));
-        }
-
-        protected static boolean isMockTick(Tuple tuple) {
-            return tuple.getSourceStreamId().equals("mockTickTuple");
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
         WindowObject wObject;
@@ -82,11 +28,11 @@ public class CummulativeMovingAvgTopology {
                 + File.separator + "log4j.properties";
 
         PropertyConfigurator.configure(log4jConfigFile);
-
+/*
         LOG.info("Testing Count Based");
         builder.setSpout("RandomInt", new RandomIntegerSpout(), 10);
         builder.setBolt("Tumbling", new TumblingWindow(wObject),1).shuffleGrouping("RandomInt");
-        builder.setBolt("Average", new MovingAverage(), 1).shuffleGrouping("Tumbling","dataStream")
+        builder.setBolt("Average", new MovingAverageBolt(), 1).shuffleGrouping("Tumbling","dataStream")
                 .shuffleGrouping("Tumbling","mockTickTuple");
 
 
@@ -108,14 +54,15 @@ public class CummulativeMovingAvgTopology {
 
         Utils.sleep(5000);
 
-
-
+*/
+        Config conf = new Config();
+        conf.setDebug(false);
         LOG.info("Testing Time Based");
         wObject = new WindowObject(5,true);
         builder = new TopologyBuilder();
         builder.setSpout("RandomInt", new RandomIntegerSpout(), 15);
         builder.setBolt("Tumbling", new TumblingWindow(wObject),1).shuffleGrouping("RandomInt");
-        builder.setBolt("Average", new MovingAverage(), 1).shuffleGrouping("Tumbling","dataStream")
+        builder.setBolt("Average", new MovingAverageBolt(), 1).shuffleGrouping("Tumbling","dataStream")
                 .shuffleGrouping("Tumbling","mockTickTuple");
 
         if (args != null && args.length > 0) {

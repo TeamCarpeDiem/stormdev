2acc1dd5a8072f24ccd3f9edd302d3cc207d7c4a
haribruno <hrajendr@andrew.cmu.edu>
2015-07-02 16:17:32 -0400

    Adding updated CF

--- examples/storm-starter/src/jvm/storm/starter/spout/RandomIntegerSpout.java
+++ examples/storm-starter/src/jvm/storm/starter/spout/RandomIntegerSpout.java
@@ -80,7 +80,7 @@ public class RandomIntegerSpout extends BaseRichSpout {
         str[9] = "Harinihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh";
         //String str1 = "HariniSachin";
         int strIndex = _rand.nextInt(10);
-        _collector.emit(new Values(str[0]));
+        _collector.emit(new Values(str[strIndex]));
     }
 
     @Override

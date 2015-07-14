dfdc4c7c056bc0a9ab0a0f4fec247b64b4ae6f05
haribruno <harini.rajendran@yahoo.com>
2015-07-14 15:54:06 -0400

    Added description to isMockTick

--- examples/storm-starter/src/jvm/storm/starter/Interfaces/IWindowBolt.java
+++ examples/storm-starter/src/jvm/storm/starter/Interfaces/IWindowBolt.java
@@ -11,5 +11,12 @@ import java.util.Map;
  * Created by Pradheep on 6/18/15.
  */
 public interface IWindowBolt {
+    /**
+     * This function should be implemented when the bolt has to detect the end of window "Mock Tick Tuple" sent by the
+     * Common Window Framework. Implementing this function is critical for functioning of the Windowing mechanism. If
+     * a bolt doesn't implement this function, then it won't be able to detect the end of windows.
+     * @param tuple The tuple which has to be evaluated
+     * @return Returns true if the tuple is a mock tick tuple else returns false
+     */
     boolean isMockTick(Tuple tuple);
 }

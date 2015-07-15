9b40b9f6b128ad683542a915f06a418abcbb51f3
haribruno <hrajendr@andrew.cmu.edu>
2015-07-15 13:23:17 -0400

    Added a method for BaseBasicBolts

--- examples/storm-starter/src/jvm/storm/starter/WindowTopologyBuilder.java
+++ examples/storm-starter/src/jvm/storm/starter/WindowTopologyBuilder.java
@@ -2,6 +2,7 @@ package storm.starter;
 
 import backtype.storm.topology.BoltDeclarer;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.topology.base.BaseRichBolt;
 import storm.starter.Interfaces.IWindowBolt;
 import storm.starter.bolt.BaseWindowBolt;
@@ -27,6 +28,17 @@ public class WindowTopologyBuilder extends TopologyBuilder {
     }
 
     public BoltDeclarer setBolt(String id, BaseRichBolt bolt, Number parallelism_hint) throws Exception {
+
+        Class<?> x = bolt.getClass();
+        BoltDeclarer obj = null;
+        if (bolt instanceof IWindowBolt){
+            obj = super.setBolt(id, bolt, parallelism_hint);
+        }
+        else throw new Exception(x + " did not implement IWindow Interface");
+        return obj;
+    }
+
+    public BoltDeclarer setBolt(String id, BaseBasicBolt bolt, Number parallelism_hint) throws Exception {
 
         Class<?> x = bolt.getClass();
         BoltDeclarer obj = null;

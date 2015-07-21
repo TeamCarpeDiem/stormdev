e11a633d792866e45c2bdf172af90a12d8415997
haribruno <harini.rajendran@yahoo.com>
2015-07-21 18:56:03 -0400

    Removed Unnecessary Comments and logs

--- examples/storm-starter/src/jvm/storm/starter/CummulativeMovingAvgTopology.java
+++ examples/storm-starter/src/jvm/storm/starter/CummulativeMovingAvgTopology.java
@@ -50,44 +50,11 @@ public class CummulativeMovingAvgTopology {
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology("test", conf, builder.createTopology());
             LOG.info("Topology Created");
-            Utils.sleep(180000);
+            Utils.sleep(60000);
             cluster.killTopology("test");
-            LOG.info("Topology Killed");
-            //cluster.shutdown();
-           // exit(0)\\;
-        }
-
-
-        Utils.sleep(5000);
-
-        wObject = new WindowObject("Sliding", 55000,2500, false);
-        //wObject = new WindowObject("Sliding", 20, 5, true); //Uncomment this line for time based window and comment the previois line
-        builder = new WindowTopologyBuilder();
-        builder.setSpout("RandomMessage", new RandomMessageSpout(), 20);
-        //wObject = new WindowObject("Sliding", 10000,2500, false);
-        wObject = new WindowObject("Sliding", 6, 2, true); //Uncomment this line for time based window and comment the previois line
-        builder = new WindowTopologyBuilder();
-        builder.setSpout("RandomMessage", new RandomMessageSpout(), 12);
-        builder.setBolt("Sliding", wObject.createWindow() ,1).shuffleGrouping("RandomMessage");
-        builder.setBolt("Average", new MovingAverageBolt(), 1).shuffleGrouping("Sliding","dataStream")
-                .shuffleGrouping("Sliding","mockTickTuple");
-
-        if (args != null && args.length > 0) {
-            conf.setNumWorkers(1);
-            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
-        }
-        else {
-
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("test", conf, builder.createTopology());
-            LOG.info("Topology Created");
-            Utils.sleep(180000);
-            Utils.sleep(35000);
-            cluster.killTopology("test");
-            LOG.info("Topology Killed");
             LOG.info("Topology Killed");
             cluster.shutdown();
-            exit(0);
+           exit(0);
         }
     }
 }
